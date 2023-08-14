898469cb71b7c7557fc4e4e8b27bbbd88de9b037
BasantiNagari <basantinagari23@gmail.com>
2023-08-14 09:05:44 +0545

    learned about var, const, let

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("Hello");
+//console.log("Hello");
+
+
+//single line comment
+/*
+multi line comment
+supper
+long
+comment*/
+
+// variables 
+// ES5 - var
+// ES6 - const, let
+//keyword var, let, const
+
+var person = "john"; 
+person = "Ram";   // resign
+
+const person1 = "jonny";  // fixed value
+
+let person2 = "basanti"; // changeable
+ person2 = "basa";
+
+console.log ({person, person1, person2})
