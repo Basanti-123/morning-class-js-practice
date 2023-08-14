ad67b45f3c529e75593d542d2ba14d5d0ba57801
BasantiNagari <basantinagari23@gmail.com>
2023-08-14 16:28:58 +0545

    1st Commit

--- index.js
+++ index.js
@@ -13,18 +13,39 @@ comment*/
 // ES6 - const, let
 //keyword var, let, const
 
-/*var person = "john"; 
+//var person = "john"; 
 person = "Ram";   // resign
 
 const person1 = "jonny";  // fixed value
 
 let person2 = "basanti"; // changeable
- person2 = "basa";
+ //person2 = "basa";
 
-console.log ({person, person1, person2})*/
+//console.log ({person, person1, person2})*/
 
 // Interactions
-alert ("js class is in session");
+/*alert ("js class is in session");
  person2 = prompt("what is your name?");
  const isLegal = confirm("are you 18 years and above?");
- console.log ({person, person1, isLegal});
+
+ console.log ({person, person1, isLegal});*/
+ 
+ // operators
+// = (Value check)
+//== (type the data type)
+//=== (strict the check inside the value uppercase and lower case string, number)
+
+alert("js class is in session")
+const age = Number(prompt("What is your age"));
+const isLegal = confirm("are you 18 years and above?");
+
+if (isLegal || person2 === "Riktim Shrestha"){
+    alert("welcome to the access ");
+}
+
+ 
+if (isLegal) {
+    alert("sorry acess denied")
+}
+
+console.log ({person1, person2, isLegal});
