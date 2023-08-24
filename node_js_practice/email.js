75311a17a8b24a453fbed0caa913d0106829f738
BasantiNagari <basantinagari23@gmail.com>
2023-08-24 22:27:34 +0545

    email.js

--- node_js_practice/email.js
+++ node_js_practice/email.js
@@ -7,48 +7,70 @@
 4. html page use for email send 
 5. Email file attach
 6. personalized email by adding custom data
+   1. handlers packages
+   2. fs package (nodejs inbuilt module)
+   3. email template (welcome.html)
+   4. using fs, read the email tamplate by method readfileSync("./welcome .html")
+   5. convert that buffer to string using to.String()
+   6. add the {{name}} in html file
+   7. compile and create tamplate  tamplate from the code using handlers.compile(emailstring)
+   8. add your custom data in the tamplate (const msg = template(data))
+   9. use that msg in the messageOptions
 */
 const nodemailer = require("nodemailer");
+const fs = require("fs");
+const Handlebars = require("handlebars");
+
 const transporter = nodemailer.createTransport({
-  host: "smtp.forwardemail.net",
+  host: "smtp.gmail.com",
   port: 465,
   secure: true,
   auth: {
     // TODO: replace `user` and `pass` values from <https://forwardemail.net>
     user: "bnagari154@gmail.com",
-    pass: "basanti123",
+    pass: "hlgagqlwtyjfpnab",
   },
 });
-nodemailer.createTransport({
-  host: "smtp.example.com",
-  port: 587,
-  secure: false, // upgrade later with STARTTLS
-  auth: {
-    user: "username",
-    pass: "password",
-  },
+//verify connection configuration
+transporter.verify((error, success) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Server is ready to take our messages");
+  }
 });
 
-const main = async (messageOptions) => {};
-
-async function main() {
+const main = async (messageOptions) => {
   // send mail with defined transport object
-  const info = await transporter.sendMail({
-    from: '"Fred Foo 👻" <foo@example.com>', // sender address
-    to: "bar@example.com, baz@example.com", // list of receivers
-    subject: "Hello ✔", // Subject line
-    text: "Hello world?", // plain text body
-    html: "<b>Hello world?</b>", // html body
-  });
+  const info = await transporter.sendMail(messageOptions);
 
   console.log("Message sent: %s", info.messageId);
-  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+};
 
-  //
-  // NOTE: You can go to https://forwardemail.net/my-account/emails to see your email delivery status and preview
-  //       Or you can use the "preview-email" npm package to preview emails locally in browsers and iOS Simulator
-  //       <https://github.com/forwardemail/preview-email>
-  //
-}
+const emailTemplate = fs.readFileSync("./welcome.html"); // Buffer
+const emailString = emailTemplate.toString();
+const template = Handlebars.compile(emailString);
 
-main().catch(console.error);
+const data = { name: "Basanti" };
+
+const messageOptions = {
+  from: '"Basanti Nagri " <bnagari154@gmail.com>', // sender address
+  to: "bnagari154@gmail.com", // list of receivers
+  subject: "Hello ✔", // Subject line
+  text: "Hello world?", // plain text body
+  html: template(data),
+  attachments: [
+    {
+      // utf-8 string as an attachment
+      filename: "hello.txt",
+      content: "hello world!",
+    },
+    {
+      // binary buffer as an attachment
+      filename: "hello2.txt",
+      content: Buffer.from("hello world! Basanti ", "utf-8"),
+    },
+  ],
+};
+
+main(messageOptions).catch(console.error);
